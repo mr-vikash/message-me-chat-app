4182f659ffec1704d3fcd1501415277c25690884
mr-vikash <mrvikash700@gmail.com>
2024-08-28 16:10:54 +0530

    login functionality added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(user_name: params[:session][:user_name])
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "You have logged in successfully"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = "Username or Password is incorrect"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You logged out successfully"
+    redirect_to login_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :user_name, presence: true, length: { minimum: 3, maximum: 12}, uniqueness: { case_sensitive: false }
+  validates :user_name, presence: true, length: { minimum: 3, maximum: 12 }, uniqueness: { case_sensitive: false }
   has_many :messages
   has_secure_password
 end
